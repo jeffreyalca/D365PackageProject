8e738cc36ce49026cb979b1a85b255e456ca1582
Jeffrey Alca <jeffrey.alca@essilor.com>
2022-06-20 20:32:43 +0800

    initial

---
+++ D365PackageProject/PreOperationFormatPhoneCreateUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using System.Text.RegularExpressions;
+
+namespace D365PackageProject
+{
+    public class PreOperationFormatPhoneCreateUpdate: IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (!context.InputParameters.ContainsKey("Target"))
+                throw new InvalidPluginExecutionException("No target found");
+
+
+            var entity = context.InputParameters["Target"] as Entity;
+
+            if (!entity.Attributes.Contains("telephone1"))
+                return;
+
+            string phoneNumber = (string)entity["telephone1"];
+            var formattedNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+            entity["telephone1"] = formattedNumber;
+        }
+    }
+}
